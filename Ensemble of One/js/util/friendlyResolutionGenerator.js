7117921d97fdc6948c8f2099de7c1f954ded6cce
Aaron Mertzenich <mertzi52101@hotmail.com>
2015-11-22 01:13:36 -0600

    MediaComposition prep and XML cleanup
    Changes include:
    - Basic preparations for implementing the Windows 10 MediaComposition
    API (filler images, Ensemble.FileIO organization).
    - Converted project creation code to use native JS implementation to
    generate an XML document (rather than the 3rd-party library that was
    previously used). This change has not yet been made to other save/load
    sections of the app, so project loading is broken for the time being.
    - Corrected "2160p" to read "UHD 4K" (which is a more useful description
    and is the common convention for that resolution).

--- Ensemble of One/js/util/friendlyResolutionGenerator.js
+++ Ensemble of One/js/util/friendlyResolutionGenerator.js
@@ -8,7 +8,7 @@
             /// <param name=height" type="Number">The width of the video.</param>
             /// <returns type="String">A string representing the resolution.</returns>
             if (width == 7680 && height == 4320) return "4320p";
-            if (width == 3840 && height == 2160) return "2160p";
+            if (width == 3840 && height == 2160) return "UHD 4K";
             if (width == 1920 && height == 1080) return "1080p";
             if (width == 1280 && height == 720) return "720p";
             if ((width == 852 || width == 720 || width == 704 || width == 640) && height == 480) return "480p";
